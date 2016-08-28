ac5e23877d326b979bb9c665a2433f5646b60b1c
robinzhou <fengguiyushao@gmail.com>
2016-08-29 01:07:06 +0800

    1

--- spring/src/main/java/concert/ConcertConfig.java
+++ spring/src/main/java/concert/ConcertConfig.java
@@ -1,9 +1,8 @@
 package concert;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 /**
  * Created by robinzhou on 2016/8/28.
