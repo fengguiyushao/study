bc01263075945eab9729b2cb47f0ed5ccc41c5cc
robinzhou <fengguiyushao@gmail.com>
2016-08-29 00:19:14 +0800

    11

--- spring/src/main/java/concert/EncoreableIntroduce.java
+++ spring/src/main/java/concert/EncoreableIntroduce.java
@@ -3,6 +3,7 @@ package concert;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.DeclareParents;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,8 +16,13 @@ public class EncoreableIntroduce {
     @DeclareParents(value = "concert.Performance+", defaultImpl = DefaultEncoreable.class)
     public static Encoreable encoreable;
 
-    @Before("execution(* concert.Performance.perform(..)) && target(encoreable)")
-    public void recordUsage(Encoreable encoreable) {
+    @Pointcut("execution(* concert.Performance.perform(int)) && args(times)")
+    public void performance(int times) {
+    }
+
+    @Before("performance(times) && this(encoreable)")
+    public void recordUsage (Encoreable encoreable, int times) {
+        System.out.println("times:" + times);
         encoreable.performEncore();
     }
 }
