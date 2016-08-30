782a25dd3a1565811d8d70ce12c143e8761e1978
robinzhou <fengguiyushao@gmail.com>
2016-08-31 00:55:43 +0800

    strange test

---
+++ spring/src/main/java/spittr/data/SpittleRepository.java
@@ -0,0 +1,13 @@
+package spittr.data;
+
+import spittr.Spittle;
+
+import java.util.List;
+
+/**
+ * Created by robinzhou on 2016/8/30.
+ */
+public interface SpittleRepository {
+
+    List<Spittle> findSpittles(long max, int count);
+}
---
+++ spring/src/main/java/spittr/web/SpittleController.java
@@ -0,0 +1,29 @@
+package spittr.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import spittr.data.SpittleRepository;
+
+/**
+ * Created by robinzhou on 2016/8/31.
+ */
+@Controller
+@RequestMapping("/spittles")
+public class SpittleController {
+
+    private SpittleRepository spittleRepository;
+
+    @Autowired
+    public SpittleController(SpittleRepository spittleRepository) {
+        this.spittleRepository = spittleRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String spittles(Model model) {
+        model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+        return "spittles";
+    }
+}
