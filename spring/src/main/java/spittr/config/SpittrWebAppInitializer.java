b84432ed488e4863066a83ddc475e934a18ce264
robinzhou <fengguiyushao@gmail.com>
2016-08-30 09:01:59 +0800

    mvc start
    mvc start

---
+++ spring/src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -0,0 +1,23 @@
+package spittr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by robinzhou on 2016/8/29.
+ */
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
