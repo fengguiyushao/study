284d60f57d89f1c1d7fd16f2cfe984365c604cff
robinzhou <fengguiyushao@gmail.com>
2016-09-01 00:44:40 +0800

    strange test

--- spring/src/main/java/spittr/data/SpitterRepository.java
+++ spring/src/main/java/spittr/data/SpitterRepository.java
@@ -1,7 +1,13 @@
 package spittr.data;
 
+import spittr.Spitter;
+
 /**
  * Created by robinzhou on 2016/8/31.
  */
 public interface SpitterRepository {
+
+    Spitter save(Spitter spitter);
+
+    Spitter findByUsername(String username);
 }
--- spring/src/test/java/spittr/web/SpitterControllerTest.java
+++ spring/src/test/java/spittr/web/SpitterControllerTest.java
@@ -1,13 +1,28 @@
 package spittr.web;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import spittr.Spitter;
+import spittr.config.RootConfig;
+import spittr.config.SpittrWebAppInitializer;
+import spittr.config.WebConfig;
 import spittr.data.SpitterRepository;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
  * Created by robinzhou on 2016/8/31.
@@ -16,7 +31,8 @@ public class SpitterControllerTest {
 
     @Test
     public void shouldShowRegistration() throws Exception {
-        SpitterController controller = new SpitterController();
+        SpitterRepository mockRepository = mock(SpitterRepository.class);
+        SpitterController controller = new SpitterController(mockRepository);
         MockMvc mockMvc = standaloneSetup(controller).build();
 
         mockMvc.perform(get("/spitter/register"))
@@ -26,6 +42,20 @@ public class SpitterControllerTest {
     @Test
     public void shouldProcessRegistration() throws Exception {
         SpitterRepository mockRepository = mock(SpitterRepository.class);
+        Spitter unsaved = new Spitter("jbauer", "24hours", "jack", "bauer");
+        Spitter saved = new Spitter(24l, "jbauer", "24hours", "jack", "bauer");
+
+        when(mockRepository.save(unsaved)).thenReturn(saved);
+
+        SpitterController controller = new SpitterController(mockRepository);
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(post("/spitter/register")
+                .param("firstName", "jack")
+                .param("lastName", "Bauer")
+                .param("username", "jbauer")
+                .param("password", "24hours"))
+                .andExpect(redirectedUrl("/spitter/jbauer"));
 
     }
 
