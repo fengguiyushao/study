0d51d4e5e555643b373e4fa71b8724e16851a520
fengguiyushao <fengguiyushao@gmail.com>
2019-12-30 23:03:20 +0800

    1

---
+++ simpleframework/src/main/java/com/robinzhou/simpleframework/helper/AopHelper.java
@@ -0,0 +1,12 @@
+package com.robinzhou.simpleframework.helper;
+
+import com.robinzhou.simpleframework.annotation.Aspect;
+
+import java.util.Set;
+
+public class AopHelper {
+
+    private static Set<Class<?>> createTargetClassSet(Aspect aspect) throws Exception {
+
+    }
+}
--- simpleframework/src/main/java/com/robinzhou/simpleframework/helper/ClassHelper.java
+++ simpleframework/src/main/java/com/robinzhou/simpleframework/helper/ClassHelper.java
@@ -1,10 +1,11 @@
 package com.robinzhou.simpleframework.helper;
 
 
-import com.robinzhou.simpleframework.util.ClassUtil;
-import com.robinzhou.simpleframework.annotation.Service;
 import com.robinzhou.simpleframework.annotation.Controller;
+import com.robinzhou.simpleframework.annotation.Service;
+import com.robinzhou.simpleframework.util.ClassUtil;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -47,5 +48,31 @@ public class ClassHelper {
         beanClassSet.addAll(getServiceClassSet());
         beanClassSet.addAll(getControllerClassSet());
         return beanClassSet;
+    }
+
+    /**
+     * 获取应用包名下某父类（或接口）的所有子类（或实现类）
+     */
+    public static Set<Class<?>> getClassSetBySuper(Class<?> superClass) {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (superClass.isAssignableFrom(cls) && !superClass.equals(cls)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下带有某注解的所有类
+     */
+    public static Set<Class<?>> getClassSetByAnnotation(Class<? extends Annotation> annotationClass) {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(annotationClass)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
     }
 }
